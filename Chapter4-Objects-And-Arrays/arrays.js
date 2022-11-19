44d7b2e34fa9d70e0593e50757346dec0aced67b
yinminaung <yinminaung72@gmail.com>
2022-11-19 23:46:16 +0630

    arrays in js

--- Chapter4-Objects-And-Arrays/arrays.js
+++ Chapter4-Objects-And-Arrays/arrays.js
@@ -2,5 +2,31 @@ let listOfNumbers = [2, 3, 5, 7, 11];
 
 console.log(listOfNumbers);
 console.log(listOfNumbers[2]);
-console.log(listOfNumbers[0]);
+console.log(listOfNumbers[0]);   
+
+// the code that returns undefined
 console.log(listOfNumbers[-3]);
+console.log(listOfNumbers[20]);
+
+// adding new items 
+listOfNumbers.push(16);     // at the end of the array
+console.log(listOfNumbers); 
+listOfNumbers.unshift(0);  // at the start of the array
+console.log(listOfNumbers);
+
+// removing new items
+listOfNumbers.pop();       // at the end of the array
+console.log(listOfNumbers);
+listOfNumbers.shift();
+console.log(listOfNumbers);
+
+// concatenating
+let l1 = [0, 1, 2, 3];
+let l2 = [4, 5, 6, 7];
+let l3 = l1 + l2; //it leads to a string
+let l4 = l1.concat(l2);
+console.log(l3);
+console.log(typeof l3);
+console.log(l4);
+console.log(typeof l4);
+
---
+++ Chapter4-Objects-And-Arrays/zero-based-counting.js
@@ -0,0 +1,19 @@
+// just my theory and thinking
+
+/* Zero-based counting is very common in programming.
+   It is used in counting the characters of a string or 
+   elements in a array. 
+*/
+
+// So Why? 
+
+// here is what i think
+
+let arr = [1, 2, 3, 4];
+
+// In C programming language, 
+// int arr[4] = {1, 2, 3, 4};
+
+// In the memory, arr stores the address of the memory
+//  of the first item in the array,
+ 
